6a541169fe3067e23159bcea85661aad653dddc6
boputu <borjabussiness@gmail.com>
2024-01-14 12:52:50 +0100

    Major fixes, improving packs section, anual and monthly button, some styles

--- bigbuy-landing-page/src/App.js
+++ bigbuy-landing-page/src/App.js
@@ -1,41 +1,93 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PackCard from './components/PackCard'; //importing the component used for every pack.
 import './styles.css';
 
 const App = () => {
-  //Custom data for the packs
-  const packs = [
-    {
-      title: 'Marketplace',
-      description: 'Para clientes que quieren vender en marketplaces o ecommerce',
-      price: '99.99 ejemplo',
-      features: ['EJEMPLO 1', 'EJEMPLO 2', 'EJEMPLO 3', 'EJEMPLO 4', 'EJEMPLO 5', 'EJEMPLO 6', 'EJEMPLO 7', 'EJEMPLO 8', 'EJEMPLO 9'],
-      ctaText: 'Personaliza tu Experiencia',
-    },
-    {
-      title: 'Ecommerce',
-      description: 'Para clientes que quieren vender en ecommerce',
-      price: '99.99 ejemplo',
-      features: ['EJEMPLO 1', 'EJEMPLO 2', 'EJEMPLO 3', 'EJEMPLO 4', 'EJEMPLO 5', 'EJEMPLO 6', 'EJEMPLO 7', 'EJEMPLO 8', 'EJEMPLO 9'],
-      ctaText: 'Personaliza tu Experiencia',
-    },
-    {
-      title: 'B2B',
-      description: 'Para clientes que quieren iniciarse en el dropshipping o realizar compras al por mayor',
-      price: '99.99 ejemplo',
-      features: ['EJEMPLO 1', 'EJEMPLO 2', 'EJEMPLO 3', 'EJEMPLO 4', 'EJEMPLO 5', 'EJEMPLO 6', 'EJEMPLO 7', 'EJEMPLO 8', 'EJEMPLO 9'],
-      ctaText: 'Personaliza tu Experiencia',
-    },
-  ];
+
+  const [paymentType, setPaymentType] = useState('annual');
+  const [prices, setPrices] = useState({});
+
+  const handlePaymentTypeChange = (type) => { //Function to put the setPaymentType() to annual or monthly. (type) must be a string.
+    setPaymentType(type);
+  };
+
+  // eslint-disable-next-line
+  const updatePrices = (type) => {
+    const updatedPrices = {};
+
+    packs.forEach((pack) => {
+      if (type === 'monthly' && typeof pack.price === 'number') {
+        // Monthly price, without discount
+        updatedPrices[pack.title.toLowerCase()] = pack.price;
+      } else if (type === 'annual' && typeof pack.price === 'number') {
+        // Annual price, with 17% discount
+        updatedPrices[pack.title.toLowerCase()] = pack.price * 0.83;
+      } else {
+        // On b2b we use a string, so we can't make a discount. ('Sin cuota mensual')
+        updatedPrices[pack.title.toLowerCase()] = pack.price;
+      }
+    });
+
+    setPrices(updatedPrices); //Updating prices on view
+  };
+
+  useEffect(() => {
+    //Force the view to render the prices each time we load the page or by clicking an annual/month button.
+    updatePrices(paymentType);
+  }, [paymentType, updatePrices]);
+
+    //Custom data for the packs
+    const packs = [
+      {
+        title: 'Marketplace',
+        description: 'Para clientes que quieren vender en marketplaces o ecommerce',
+        price: 119.00,
+        features: ['EJEMPLO 1', 'EJEMPLO 2', 'EJEMPLO 3', 'EJEMPLO 4', 'EJEMPLO 5', 'EJEMPLO 6', 'EJEMPLO 7', 'EJEMPLO 8', 'EJEMPLO 9'],
+        ctaText: 'Personaliza tu Experiencia',
+        image: 'marketplace.jpg',
+      },
+      {
+        title: 'Ecommerce',
+        description: 'Para clientes que quieren vender en ecommerce',
+        price: 89.00,
+        features: ['EJEMPLO 1', 'EJEMPLO 2', 'EJEMPLO 3', 'EJEMPLO 4', 'EJEMPLO 5', 'EJEMPLO 6', 'EJEMPLO 7', 'EJEMPLO 8', 'EJEMPLO 9'],
+        ctaText: 'Personaliza tu Experiencia',
+        image: 'ecommerce.jpg',
+      },
+      {
+        title: 'B2B',
+        description: 'Para clientes que quieren iniciarse en el dropshipping o realizar compras al por mayor',
+        price: 'Sin cuota mensual',
+        features: ['EJEMPLO 1', 'EJEMPLO 2', 'EJEMPLO 3', 'EJEMPLO 4', 'EJEMPLO 5', 'EJEMPLO 6', 'EJEMPLO 7', 'EJEMPLO 8', 'EJEMPLO 9'],
+        ctaText: 'Personaliza tu Experiencia',
+        image: 'b2b.jpg',
+      },
+    ];
 
   return (
     //Importing the component PackCard with their properties based on the packs array.
     //It will show on the view as many packs as the packs array have.
+    //This will check if paymentType is monthly or annual and will give a .active.
     <div className="app">
       <h1>Active el pack mayorista que mejor se ajuste a su negocio</h1>
+      <div className="payment-buttons">
+        <button
+          className={`payment-button ${paymentType === 'monthly' ? 'active' : ''}`}
+          onClick={() => handlePaymentTypeChange('monthly')}
+        >
+          Pago mensual
+        </button>
+        <button
+          className={`payment-button ${paymentType === 'annual' ? 'active' : ''}`}
+          onClick={() => handlePaymentTypeChange('annual')}
+        >
+          Pago anual <br />
+          <span style={{ fontSize: `10px` }}>Ahorro 17%</span>
+        </button>
+      </div>
       <div className="packs-container">
         {packs.map((pack, index) => (
-          <PackCard key={index} {...pack} />
+          <PackCard key={index} {...pack} prices={prices} />
         ))}
       </div>
     </div>
--- bigbuy-landing-page/src/components/PackCard.js
+++ bigbuy-landing-page/src/components/PackCard.js
@@ -1,24 +1,64 @@
 import React, { useState } from 'react';
 
-const PackCard = ({ title, description, price, features, ctaText }) => {
+// Importing images
+import marketplaceImage from '../images/marketplace.jpg';
+import ecommerceImage from '../images/ecommerce.jpg';
+import b2bImage from '../images/b2b.jpg';
+
+//This is made beacause the error loading images directly from the relative path, so we check if is the one we want and forces it.
+const getImagePath = (imageName) => {
+  switch (imageName) {
+    case 'marketplace.jpg':
+      return marketplaceImage;
+    case 'ecommerce.jpg':
+      return ecommerceImage;
+    case 'b2b.jpg':
+      return b2bImage;
+    default:
+      return null;
+  }
+};
+
+const PackCard = ({ title, description, price, features, ctaText, image, prices }) => {
+
+    //This function is made because b2b is not a number and we don't want to change the price of b2b after clicking on monthly / annual.
+    const formatPrice = (price) => {
+        if (typeof price === 'number') {
+          return `${price.toFixed(2)}€`;
+        } else {
+          return price;
+        }
+      };
 
   const [showAllFeatures, setShowAllFeatures] = useState(false); //This option handles the "Ver más/menos info." button
   const displayedFeatures = showAllFeatures ? features : features.slice(0, 5); //This options decides how many features its shown (default 5).
 
+  //imagePath saves the exact location of each image (ex: '../images/marketplace.jpg')
+  const imagePath = getImagePath(image);
+
   return (
     <div className="pack-card">
-      <h2>{title}</h2>
+      <div className="title-container">
+        <div className="background-image" style={{ backgroundImage: `url(${imagePath})` }}>
+         <h2 className="image-text">{title}</h2>
+        </div>
+      </div>
       <p>{description}</p>
-      <p>Price: {price}</p>
+      <div className='pricing'>
+       <span className="pricingText">{formatPrice(prices[title.toLowerCase()])}</span>
+       {title !== 'B2B' && <span> / mes</span>}
+      </div>
+      <p>Cuota de alta 89,00 €</p>
       <button className='ctaButton'>{ctaText}</button>
-      <ul> {/* This feature scrolls through the list of features */}
+      <hr className="vertical-line"></hr>
+      <ul> {/* This function scrolls through the list of features */}
         {displayedFeatures.map((feature, index) => (
           <li key={index}>{feature}</li>
         ))}
       </ul>
       {/* This button has the function to change the setShowAllFeatures() to true or false to know what text to show*/}
       <button onClick={() => setShowAllFeatures(!showAllFeatures)}>
-        {showAllFeatures ? 'Ver menos info' : 'Ver más info'}
+        {showAllFeatures ? 'Ver menos detalles' : 'Ver más detalles'}
       </button>
     </div>
   );
